68d2db87802891bee302e0e874afdd7af677669e
Winston Spencer Sonnesen <sonnesen@gmail.com>
2024-01-30 22:15:22 -0300

    Implementar calculador com menu básico

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+import calculadora.Calculadora;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-     
+  public static void main(String[] args) throws Exception {
 
+    Calculadora calculadora = new Calculadora();
+    int soma = calculadora.somarDoisNumeros(2, 2);
+    int subtracao = calculadora.subtrairDoisNumeros(3, 2);
+    int multiplicacao = calculadora.multiplicarDoisNumeros(4, 2);
+    int divisao = calculadora.dividirDoisNumeros(8, 2);
 
-    }
+    System.out.println("Soma: %s".formatted(soma));
+    System.out.println("Subtração: %s".formatted(subtracao));
+    System.out.println("Multiplicação: %s".formatted(multiplicacao));
+    System.out.println("Divisão: %s".formatted(divisao));
+  }
 }
